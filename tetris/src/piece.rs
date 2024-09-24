d5424adceb174becf9d5f6a7110f9b1a11c70dfe
n4mlz <n4mlz.dev@gmail.com>
2024-09-25 00:29:32 +0900

    :sparkles: rotation state

--- tetris/src/piece.rs
+++ tetris/src/piece.rs
@@ -8,3 +8,34 @@ pub enum Piece {
     O,
     T,
 }
+
+// TODO: put in order
+
+pub enum RotationState {
+    North,
+    East,
+    South,
+    West,
+}
+
+impl RotationState {
+    pub fn rotate_left(&self) -> RotationState {
+        use RotationState::*;
+        match self {
+            North => West,
+            East => North,
+            South => East,
+            West => South,
+        }
+    }
+
+    pub fn rotate_right(&self) -> RotationState {
+        use RotationState::*;
+        match self {
+            North => East,
+            East => South,
+            South => West,
+            West => North,
+        }
+    }
+}

f34fe1654c54f0a5bd8c4d7284ce5ef5333864e4
n4mlz <n4mlz.dev@gmail.com>
2024-09-24 23:17:15 +0900

    :sparkles: struct LastAction

--- tetris/src/state.rs
+++ tetris/src/state.rs
@@ -1,10 +1,46 @@
 use crate::*;
 
-struct State {
-    board: Box<dyn Board>,
-    current: piece::Piece,
-    hold: Option<Piece>,
-    next: Vec<Piece>,
-    ren: u32,
-    b2b: bool,
+pub struct State {
+    pub board: Box<dyn Board>,
+    pub current: piece::Piece,
+    pub hold: Option<Piece>,
+    pub next: Vec<Piece>,
+    pub combo: u32,
+    pub b2b: bool,
+    pub last_action: LastAction,
+}
+
+pub struct LastAction {
+    pub placement_kind: PlacementKind,
+    pub b2b: bool,
+    pub combo: u32,
+    pub perfect_clear: bool,
+    pub garbage_sent: u32,
+    pub time: u32,
+    pub operations: Vec<Operation>,
+}
+
+pub enum PlacementKind {
+    None,
+    Clear1,
+    Clear2,
+    Clear3,
+    Clear4,
+    MiniTspin,
+    MiniTspin1,
+    MiniTspin2,
+    Tspin,
+    Tspin1,
+    Tspin2,
+    Tspin3,
+}
+
+pub enum Operation {
+    MoveLeft,
+    MoveRight,
+    SoftDrop,
+    HardDrop,
+    RotateLeft,  // Counter-clockwise
+    RotateRight, // Clockwise
+    Hold,
 }

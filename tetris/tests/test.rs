ccae93afa9a5d86b2f76743a818cd259df6da792
n4mlz <n4mlz.dev@gmail.com>
2024-10-04 20:42:34 +0900

    :white_check_mark: ランダムプレイアウトのテストを追加

--- tetris/tests/test.rs
+++ tetris/tests/test.rs
@@ -1,25 +1,78 @@
 use crate::tetris::FieldCells;
 use rand::{seq::SliceRandom, thread_rng};
-use std::collections::VecDeque;
+use std::{
+    collections::VecDeque,
+    hash::{DefaultHasher, Hash, Hasher},
+    thread::sleep,
+};
 use strum::IntoEnumIterator;
 extern crate tetris;
 
 // this is not a test, but for checking the display
 #[test]
-fn it_works() {
-    let mut initial_next_pieces: Vec<_> = tetris::Piece::iter().collect();
+fn random_playout() {
+    let mut next_pieces: Vec<_> = tetris::Piece::iter().collect();
     let mut rng = thread_rng();
-    initial_next_pieces.shuffle(&mut rng);
-    let mut initial_next_pieces = VecDeque::from(initial_next_pieces);
+    next_pieces.shuffle(&mut rng);
+    let mut next_pieces = VecDeque::from(next_pieces);
 
-    let initial_state = tetris::State {
+    let mut current_state = tetris::State {
         board: tetris::Board::new(),
-        current_piece: Some(initial_next_pieces.pop_front().unwrap()),
+        current_piece: Some(next_pieces.pop_front().unwrap()),
         hold_piece: None,
-        next_pieces: initial_next_pieces,
+        next_pieces,
         b2b: false,
         last_action: None,
     };
 
-    print!("{}", initial_state);
+    loop {
+        println!("{}", termion::clear::All);
+        println!("{}", current_state);
+
+        let legal_actions = current_state.legal_actions();
+        if let Some(next_state) = legal_actions.choose(&mut rng) {
+            current_state = next_state.clone();
+            if current_state.next_pieces.len() < 8 {
+                current_state.extend_next_pieces();
+            }
+        } else {
+            break;
+        }
+        sleep(std::time::Duration::from_millis(50));
+    }
+}
+
+#[test]
+fn hash_of_movement_state() {
+    let left_movement_state = tetris::MovementState::new_from_piece(
+        tetris::Piece::I,
+        Some(tetris::Piece::T),
+        VecDeque::from(vec![tetris::Piece::L, tetris::Piece::J]),
+    );
+
+    let mut right_movement_state = left_movement_state.clone();
+
+    let mut left_hasher = DefaultHasher::new();
+    let mut right_hasher = DefaultHasher::new();
+
+    left_movement_state.hash(&mut left_hasher);
+    right_movement_state.hash(&mut right_hasher);
+
+    assert_eq!(left_hasher.finish(), right_hasher.finish());
+
+    right_movement_state
+        .movements_history
+        .push(tetris::PieceMovement::MoveRight);
+    right_movement_state.time += 1;
+
+    let mut right_hasher = DefaultHasher::new();
+    right_movement_state.hash(&mut right_hasher);
+
+    assert_eq!(left_hasher.finish(), right_hasher.finish());
+
+    let right_movement_state = right_movement_state.hold();
+    let mut right_hasher = DefaultHasher::new();
+    right_movement_state.hash(&mut right_hasher);
+
+    assert_ne!(left_hasher.finish(), right_hasher.finish());
 }

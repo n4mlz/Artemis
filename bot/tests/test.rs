d5bbb1c4e2e4bc4de90c2e1ac87d2aa488ccf8da
n4mlz <n4mlz.dev@gmail.com>
2024-10-17 18:46:03 +0900

    :white_check_mark: bot にゲームをプレイさせるテストを追加

--- bot/tests/test.rs
+++ bot/tests/test.rs
@@ -1,5 +1,5 @@
 use rand::{seq::SliceRandom, thread_rng};
-use std::{collections::VecDeque, thread::sleep};
+use std::collections::VecDeque;
 use strum::IntoEnumIterator;
 use tetris::FieldCells;
 
@@ -26,4 +26,40 @@ fn search_halting() {
     let next_state = bot.search(state.clone()).unwrap();
 
     assert!(next_state != state);
+}
+
+// this is not a test, but for checking the display
+#[test]
+fn random_playout() {
+    let mut next_pieces: Vec<_> = tetris::Piece::iter().collect();
+    let mut rng = thread_rng();
+    next_pieces.shuffle(&mut rng);
+    let mut next_pieces = VecDeque::from(next_pieces);
+
+    let mut current_state = tetris::State {
+        board: tetris::Board::new(),
+        current_piece: Some(next_pieces.pop_front().unwrap()),
+        hold_piece: None,
+        next_pieces,
+        b2b: false,
+        last_action: None,
+    };
+
+    let bot = bot::Bot {
+        evaluator: bot::Evaluator {},
+    };
+
+    loop {
+        println!("{}", termion::clear::All);
+        println!("{}", current_state);
+
+        if let Some(next_state) = bot.search(current_state.clone()) {
+            current_state = next_state.clone();
+            if current_state.next_pieces.len() < 8 {
+                current_state.extend_next_pieces();
+            }
+        } else {
+            break;
+        }
+    }
 }

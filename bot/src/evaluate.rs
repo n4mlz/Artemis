573343b9751ea5ec8b1de2c305221734b8b921c5
n4mlz <n4mlz.dev@gmail.com>
2024-10-17 14:11:00 +0900

    :sparkles: 仮の Evaluator を実装

--- bot/src/evaluate.rs
+++ bot/src/evaluate.rs
@@ -1,1 +1,18 @@
 use crate::*;
+use rand::Rng;
+use tetris::State;
+
+pub type Reward = i32;
+pub type Value = i32;
+
+pub struct Evaluator {
+    // TODO: add some parameters
+}
+
+impl Evaluator {
+    pub fn evaluate(&self, state: &State) -> (Reward, Value) {
+        // TODO: implement
+        let mut rng = rand::thread_rng();
+        (rng.gen_range(0..100), rng.gen_range(0..100))
+    }
+}

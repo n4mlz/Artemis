50136c6d7774c51106b455438d1a54eb454a8cdc
n4mlz <n4mlz.dev@gmail.com>
2024-11-08 12:16:09 +0900

    :sparkles: threadpool を追加

--- optimizer/src/main.rs
+++ optimizer/src/main.rs
@@ -2,11 +2,13 @@ mod battle;
 mod mutate;
 mod optimizer;
 mod score;
+mod threadpool;
 
 pub use battle::*;
 pub use mutate::*;
 pub use optimizer::*;
 pub use score::*;
+pub use threadpool::*;
 
 fn main() {
     let path = "population.json";

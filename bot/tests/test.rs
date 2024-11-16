87ca524d6a101a2b8f510fcf7b6a77b1194c80a6
n4mlz <n4mlz.dev@gmail.com>
2024-11-16 22:38:43 +0900

    :white_check_mark: 時間基準で評価値を得るテストを追加

--- bot/tests/test.rs
+++ bot/tests/test.rs
@@ -1,4 +1,5 @@
 const EVALUATOR_REPEAT: u32 = 10;
+const EVALUATOR_TIME: u32 = 1000;
 
 #[test]
 fn search_halting() {
@@ -17,7 +18,7 @@ fn search_halting() {
 
 // this is not a test, but for checking the display
 #[test]
-fn bot_play() {
+fn bot_play_for_count() {
     let mut current_state = tetris::State::new_random_state();
 
     let bot = bot::Bot {
@@ -30,6 +31,31 @@ fn bot_play() {
         bot::debug_evaluation(&current_state);
 
         if let Some(next_state) = bot.get_move_for_count(current_state.clone(), EVALUATOR_REPEAT) {
+            current_state = next_state.clone();
+            if current_state.next_pieces.len() < 8 {
+                current_state.extend_next_pieces();
+            }
+        } else {
+            break;
+        }
+    }
+}
+
+// this is not a test, but for checking the display
+#[test]
+fn bot_play_for_time() {
+    let mut current_state = tetris::State::new_random_state();
+
+    let bot = bot::Bot {
+        evaluator: bot::Evaluator::default(),
+    };
+
+    loop {
+        println!("{}", termion::clear::All);
+        println!("{}", current_state);
+        bot::debug_evaluation(&current_state);
+
+        if let Some(next_state) = bot.get_move_for_time(current_state.clone(), EVALUATOR_TIME) {
             current_state = next_state.clone();
             if current_state.next_pieces.len() < 8 {
                 current_state.extend_next_pieces();
